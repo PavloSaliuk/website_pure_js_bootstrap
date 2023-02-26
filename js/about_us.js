1e297715f510f44bfa42958cf7e81d80c0425b31
05122022 <pavlo.saliuk@gmail.com>
2023-02-26 14:24:38 +0200

    in accordion block - change title every 3 seconds and name of all function

--- js/about_us.js
+++ js/about_us.js
@@ -1,12 +1,9 @@
 (() => {
-
     function updateClock() {
-        const clockContainer = document.querySelector('.about_us_title_clock');
-        clockContainer.innerText = ('Today is : ' + new Date().toLocaleString('uk'));
+      const clockContainer = document.querySelector(".about_us_title_clock");
+      clockContainer.innerText = "Today is : " + new Date().toLocaleString("uk");
     }
-
+  
     setInterval(updateClock, 1000);
-    setTimeout(() => {
-    } , 1000);
-
-})();
+    setTimeout(() => {}, 1000);
+  })();
--- js/accordion.js
+++ js/accordion.js
@@ -1,23 +1,17 @@
 (() => {
-
-    const messages = [
-        'Make your choise',
-        'Meet your beer',
-        'Meet your beer and make your choise',
-        'Share with your friends'
-    ]
-    function getGreeting() {
-        const index = Math.floor(Math.random() * messages.length);
-        return messages[index];
+    const textParagraph = [
+      "Make your choise",
+      "Meet your beer",
+      "Meet your beer and make your choise",
+      "Share with your friends",
+    ];
+    function getRandomParagraph() {
+      const index = Math.floor(Math.random() * textParagraph.length);
+      return textParagraph[index];
     }
-    function changeTitle() {
-        const title = document.querySelector('.choise__paragraph_question')
-        title.innerHTML = getGreeting();
+    function changeChoiseParagraph() {
+      const title = document.querySelector(".choise__paragraph_question");
+      title.innerHTML = getRandomParagraph();
     }
-    // function changeChoiseParagraph() {
-    //     let choiseTitle = document.querySelector('.choise__paragraph_question');
-    //     choiseTitle.innerText = "Meet your beer and make your choise"; 
-    // }
-    setInterval(changeTitle, 4000)
-}
-)();
+    setInterval(changeChoiseParagraph, 3000);
+  })();

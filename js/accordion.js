226ab5c3674eec66cf1e3d3e8ce934a5c9e47fd6
05122022 <pavlo.saliuk@gmail.com>
2023-02-26 13:46:49 +0200

    create js in accordion block - change title every 4 seconds

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,23 @@
-(function () {
+(() => {
 
-    let choiseTitle = document.querySelector('.choise__paragraph_question');
-    choiseTitle.innerText = "Make Your Choice ans Meet Our Beer."; 
-
-})();
+    const messages = [
+        'Make your choise',
+        'Meet your beer',
+        'Meet your beer and make your choise',
+        'Share with your friends'
+    ]
+    function getGreeting() {
+        const index = Math.floor(Math.random() * messages.length);
+        return messages[index];
+    }
+    function changeTitle() {
+        const title = document.querySelector('.choise__paragraph_question')
+        title.innerHTML = getGreeting();
+    }
+    // function changeChoiseParagraph() {
+    //     let choiseTitle = document.querySelector('.choise__paragraph_question');
+    //     choiseTitle.innerText = "Meet your beer and make your choise"; 
+    // }
+    setInterval(changeTitle, 4000)
+}
+)();

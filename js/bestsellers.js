793d8f811aef682556162b09626bfe99b8ce2db0
kashalotik19@gmail.com <kashalotik19@gmail.com>
2023-03-01 15:45:40 +0200

    edited bestsellers js block

--- js/bestsellers.js
+++ js/bestsellers.js
@@ -1,49 +1,26 @@
-// (function () {
-//     const slides = [
-//         document.querySelector("bestseller__imperial-slide"),
-//         document.querySelector("bestseller__indian-slide"),
-//         document.querySelector("bestseller__american-slide"),
-//     ];
+(function () {
+    let slidePosition = 0;
+    const slides = document.getElementsByClassName('bestsellers__carousel');
+    const totalSlides = slides.length;
 
-//     let currentSlide = 0;
+    document.
+        getElementById('bestseller__carousel-next')
+        .addEventListener("click", function () {
+            moveToNextSlide();
+        })
+    document.
+        getElementById('bestseller__carousel-prev')
+        .addEventListener("click", function () {
+            moveToPrevSlide();
+        })
 
-//     function renderSlides(slides) {
-//         const slidesContainer = document.querySelector('bestsellers__carousel');
-//         // previous line ('.?')
-//         slidesContainer.innerHTML = slides[currentSlide];
-//     }
+    function moveToNextSlide() {
+        console.log('hello next');
+    }
+    function moveToPrevSlide() {
+        console.log('hello prev');
+    }
 
-//     function nextSlide() {
-//         currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-//         renderSlides(slides);
-//     }
-
-//     function prevSlide() {
-//         currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1;
-//         renderSlides(slides);
-//     }
-
-//     renderSlides(slides);
-
-//     const nextButton = document.querySelector('.bestseller__carousel-arrow-icon-next');
-//     nextButton.addEventListener('click', nextSlide);
-
-//     const nextButton = document.querySelector('.bestseller__carousel-arrow-icon-prew');
-//     nextButton.addEventListener('click', nextSlide);
-
-// })();
+})();
 
 
-// const slidesContainer = document.getElementById(".bestsellers");
-// const slide = document.querySelector(".bestsellers__carousel");
-// const prevButton = document.getElementById(".bestseller__carousel-arrow-icon-prev");
-// const nextButton = document.getElementById(".bestseller__carousel-arrow-icon-next");
-
-// nextButton.addEventListener("click", () => {
-//     const slideWidth = slide.clientWidth;
-//     slidesContainer.scrollLeft += slideWidth;
-// });
-// prevButton.addEventListener("click", () => {
-//     const slideWidth = slide.clientWidth;
-//     slidesContainer.scrollLeft -= slideWidth;
-// });

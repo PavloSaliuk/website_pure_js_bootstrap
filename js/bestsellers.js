53b9b9f13fc85291447e1842916fb5d76be6bad9
kashalotik19@gmail.com <kashalotik19@gmail.com>
2023-03-03 15:37:34 +0200

    edited carousel in bestsellers block

--- js/bestsellers.js
+++ js/bestsellers.js
@@ -1,47 +1,44 @@
 (function () {
     let slidePosition = 0;
-    const slides = document.getElementsByClassName('bestsellers__carousel');
+    const slides = document.getElementsByClassName("bestsellers__carousel");
     const totalSlides = slides.length;
-
-    document.
-        getElementById('bestseller__carousel-next')
-        .addEventListener("click", function () {
-            moveToNextSlide();
-        })
-    document.
-        getElementById('bestseller__carousel-prev')
-        .addEventListener("click", function () {
-            moveToPrevSlide();
-        })
-
+  
+    document
+      .getElementById("bestseller__carousel-next")
+      .addEventListener("click", function () {
+        moveToNextSlide();
+      });
+    document
+      .getElementById("bestseller__carousel-prev")
+      .addEventListener("click", function () {
+        moveToPrevSlide();
+      });
+  
     function updateSlidePosition() {
-        for (let slide of slides) {
-            slide.classList.remove('bestseller__visible-slide');
-            slide.classList.add('bestseller__hidden-slide');
-        }
-
-        slides[slidePosition].classList.add('bestseller__visible-slide');
+      for (let slide of slides) {
+        slide.classList.remove("bestseller__visible-slide");
+        slide.classList.add("bestseller__hidden-slide");
+      }
+  
+      slides[slidePosition].classList.add("bestseller__visible-slide");
     }
-
+  
     function moveToNextSlide() {
-        updateSlidePosition();
-
-        if (slidePosition === totalSlides - 1) {
-            slidePosition = 0;
-        } else {
-            slidePosition++;
-        }
+      if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+      } else {
+        slidePosition++;
+      }
+      updateSlidePosition();
     }
-
+  
     function moveToPrevSlide() {
-        updateSlidePosition();
-
-        if (slidePosition === 0) {
-            slidePosition = totalSlides - 1;
-        } else {
-            slidePosition--;
-        }
+      if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+      } else {
+        slidePosition--;
+      }
+      updateSlidePosition();
     }
-})();
-
-
+  })();
+  

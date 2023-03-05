f7f804f9d82dd43c856d4c4f1a03200715dd5363
PavelVolik <123629432+PavelVolik@users.noreply.github.com>
2023-03-05 15:39:16 +0000

    change css and js sections our partners

--- js/our_events-timer.js
+++ js/our_events-timer.js
@@ -1,33 +1,16 @@
 (() => {
-    function countdown(dateString) {
-      var endDate = new Date(dateString).getTime();
-      var x = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = endDate - now;
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.querySelector('.timer').innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-        if (distance < 0) {
-          clearInterval(x);
-          document.querySelector('.timer').innerHTML = "EXPIRED";
-        }
-      }, 1000);
-    }
-    
-    var dateElement = document.querySelector('.timer');
-    var countdownInterval = null;
-  
-    dateElement.addEventListener('mouseover', function() {
-      countdownInterval = setInterval(function() {
-        countdown('AUGUST 19, 2023');
-      }, 1000);
+    const timers = document.querySelectorAll('.timer');
+    timers.forEach((timer) => {
+        const dateString = timer.getAttribute('data-date');
+        const eventDate = new Date(dateString);
+        const diffTime = eventDate.getTime() - Date.now();
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        timer.setAttribute('data-days', diffDays);
+        timer.addEventListener('click', () => {
+            const text = timer.getAttribute('data-text').replace('X', diffDays);
+            alert(text);
+        });
     });
-  
-    dateElement.addEventListener('mouseout', function() {
-      clearInterval(countdownInterval);
-      document.querySelector('.timer').innerHTML = 'AUGUST 19, 2023';
-    });
-  })();
-  
+
+})();
+

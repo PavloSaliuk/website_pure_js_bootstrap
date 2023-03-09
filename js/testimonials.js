927a5dc9c3de8ed3de3140ff5212df1b9c6ba6ae
rosaliia-kulynska <rosalika.2002@gmail.com>
2023-03-09 12:30:14 +0200

    made corections testimonials.js

--- js/testimonials.js
+++ js/testimonials.js
@@ -6,11 +6,18 @@
 '<div><img src="img/david-bodwan.png" alt="erica maddison"></div>',
 ];
 
+let currentSlide = 0;
+
 function renderSlides(slides) {
     const slidesContainer = document.querySelector('.products-carousel__slides');
-    slidesContainer.innerHTML = '';
-    for (const slide of slides) {
-        slidesContainer.innerHTML += slide;
-    }
+    slidesContainer.innerHTML = slides[currentSlide];
+    
 }
+
+function nextSlide() {
+    currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+    renderSlides(slides);
+}
+
+setInterval(nextSlide, 2000);
 })();
